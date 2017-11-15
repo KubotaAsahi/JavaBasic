4bb06fad894b436138ce3addf25f97c8d53d97a0
kubota asahi <asahi1997829@gmail.com>
2017-11-15 18:53:13 +0900

    少しずつ問題の考え方がつかめてきた感じがします。

--- Practice/src/practice03/PTra03_04.java
+++ Practice/src/practice03/PTra03_04.java
@@ -34,5 +34,33 @@ public class PTra03_04 {
 		 */
 
 
+		if(num%4==0) {
+			if(num%100==0) {
+				if(num%400==0) {
+					System.out.println(num+"年はうるう年です。");
+				} else {
+					System.out.println(num+"年はうるう年ではありません。");
+				}
+			} else {
+				System.out.println(num+"年はうるう年です。");
+			}
+		} else {
+			System.out.println(num+"年はうるう年ではありません。");
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
