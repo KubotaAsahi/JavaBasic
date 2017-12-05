8446520cfd21c9a7d4d01b8ec4a11770c71b5135
DESKTOP-MPD6IPL\asahi <asahi1997829@gmail.com>
2017-12-05 18:04:20 +0900

    少し進みましたがまた詰まってきたので、慎重にやります

---
+++ Practice/src/practice13/common/Hero.java
@@ -0,0 +1,12 @@
+package practice13.common;
+
+public class Hero extends Character{
+
+	public Hero(int hp, int power, int endurance) {
+		super(25,10,7);
+	}
+}
+/*
+ * ★ common.Characterクラスを継承した、common.Heroクラスを作成してください
+ * 	●HP, POWER, ENDURANCEはそれぞれ、（25, 10, 7）
+ */
--- Practice/src/practice13/ptra13/PTra13_04.java
+++ Practice/src/practice13/ptra13/PTra13_04.java
@@ -6,6 +6,9 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Hero;
+import practice13.common.Slime;
+
 public class PTra13_04 {
 
 	/*
@@ -15,17 +18,32 @@ public class PTra13_04 {
 	public static void main(String[] args) {
 
 		// ★ HeroインスタンスとSlimeインスタンスを作成し、それぞれの名前に"勇者", "スライム"を設定してください
-
-
+		Hero hero = new Hero(25, 10, 7);
+		Slime slime = new Slime(10,5,2);
+		
+		hero.setName("勇者");
+		slime.setName("スライム");
 		/*
 		 * ★ HeroとSlimeを、どちらかが体力０になるまで戦わせます
 		 *
 		 * 	●Heroの攻撃 -> ダメージ判定 -> Slimeの攻撃 -> ダメージ判定
 		 * 上記を繰り返し行います
+		 * while
 		 */
-
-
+		while(true) {
+			System.out.println(hero.getName()+"の攻撃、"+slime.getName()+"に"+hero.attack()+"のダメージ");
+			hero.attack();                        
+			slime.damage(hero.attack());           
+			
+			System.out.println(slime.getName()+"の攻撃、"+hero.getName()+"に"+slime.attack()+"のダメージ");
+			slime.attack();                        
+			hero.damage(slime.attack());
+			break;
+			
+		}
 		// ★ 勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
-
+		
+		
+		
 	}
 }
